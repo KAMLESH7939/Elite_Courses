c15b8853b7a43a466ea7cdc0cd4c752eaff710b4
KAMLESH7939 <131372442+KAMLESH7939@users.noreply.github.com>
2024-11-24 23:26:40 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+//rafc....;
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+     <nav className='bg-bgDark py-4'>
+        <h1 className='text-center text-3xl font-bold text-white'> Elite Courses</h1>
+     </nav>   
+    </div>
+  )
+}
+
+export default Navbar;
